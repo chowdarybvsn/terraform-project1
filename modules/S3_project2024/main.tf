e41d2964da245d892344e607301396927767c759
chowdarybvsn <chowdarybvsn@gmail.com>
2024-01-01 17:07:19 +0530

    added some checkov bucket realted suggestions

--- modules/S3_project2024/main.tf
+++ modules/S3_project2024/main.tf
@@ -1,6 +1,19 @@
 resource "aws_s3_bucket" "bucket_project" {
     bucket = var.s3_bucket_name
 }
+resource "aws_s3_bucket_acl" "acl" {
+     bucket = aws_s3_bucket.bucket_project.bucket
+     acl = "private"
+}
+resource "aws_s3_bucket_public_access_block" "acc_block"{
+   bucket = aws_s3_bucket.bucket_project.bucket
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+
+}
 
 resource "aws_s3_bucket_ownership_controls" "example" {
   bucket = aws_s3_bucket.bucket_project.id
